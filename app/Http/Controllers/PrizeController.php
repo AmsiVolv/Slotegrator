8c23f39ee949a751eece05d9607bad25eec13be4
Vyacheslav Voloshyn <volv02@vse.cz>
2020-07-26 17:14:02 +0200

    Если не смотреть на код, то все не плохо --- Выписывая данные о играх пользователя(общую статистику)

--- app/Http/Controllers/PrizeController.php
+++ app/Http/Controllers/PrizeController.php
@@ -3,11 +3,11 @@ declare (strict_types=1);
 
 
 namespace App\Http\Controllers;
+
+use App\Models\PrizeType;
 use App\Models\Thing;
 use Faker\Generator;
-use App\Models\PrizeType;
 use Illuminate\Contracts\View\Factory;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\View\View;
 
@@ -28,18 +28,8 @@ class PrizeController extends Controller
      */
     public function getRandomPrize()
     {
-        return PrizeType::all()->where('count', '>',0)->random()->toArray();
+        return PrizeType::all()->where('count', '>', 0)->random()->toArray();
     }
-
-    /**
-     * Function to get a random item from DB
-     * @return array
-     */
-    public function getRandomItem()
-    {
-        return Thing::all()->where('status', '=', 1)->random()->toArray();
-    }
-
 
     /**
      * Check prize type function
@@ -49,34 +39,34 @@ class PrizeController extends Controller
      */
     private function checkThePrise(Array $prizeArray, Generator $fake)
     {
-       if($prizeArray['type'] !== 'thing')
-       {
-           return $this->pointMoneyCheck($prizeArray, $fake);
-       }else{
-          return $this->quantityCheck($prizeArray, 1);
-       }
+        if ($prizeArray['type'] !== 'thing') {
+            return $this->pointMoneyCheck($prizeArray, $fake);
+        } else {
+            return $this->quantityCheck($prizeArray, 1);
+        }
     }
-
-
 
     /**
      * Генерируем тут циферку и пусть себе хорошо живет, если че она заправляет тем насколько тебе везет
+     * (мне не везет)
+     *
+     *
      * @param array $prize
      * @param Generator $fake
      * @return Factory|View
      */
     private function pointMoneyCheck(Array $prize, Generator $fake)
     {
-        $amount =  $fake->numberBetween($prize['count']/15+1, $prize['count']/10+1);
+        $amount = $fake->numberBetween($prize['count'] / 15 + 1, $prize['count'] / 10 + 1);
         return $this->quantityCheck($prize, $amount);
     }
-
-
 
     /**
      * Ну тут все просто
      * Если еще система позволяет крутить казино - крутись
      * Нет - получай бонусики, так как acive=0
+     *
+     * Знал бы прикуп, жил бы в Сочи
      *
      * @param array $prizeArray
      * @param Int $amount
@@ -84,32 +74,42 @@ class PrizeController extends Controller
      */
     private function quantityCheck(Array $prizeArray, Int $amount)
     {
-        if($prizeArray['type'] !=='points'){
-            if($prizeArray['count']-$amount === 0)
-            {
+        if ($prizeArray['type'] !== 'points') {
+            if ($prizeArray['count'] - $amount === 0) {
                 DB::table('prize_types')
                     ->where('id', $prizeArray['id'])
-                    ->update(['active'=>0, 'count'=>0]);
-            }else{
+                    ->update(['active' => 0, 'count' => 0]);
+            } else {
                 DB::table('prize_types')
                     ->where('id', $prizeArray['id'])
-                    ->update(['count'=>$prizeArray['count']-$amount]);
+                    ->update(['count' => $prizeArray['count'] - $amount]);
             }
         }
-       return $this->insertToDB($prizeArray, $amount);
+        return $this->insertToDB($prizeArray, $amount);
     }
-
 
     /**
      * Поздравим с победой бонусов, ведь вещей больше нет
+     * Функция запишет твою победу в ДБ
+     * Вернет  тебя на страницу победитель
+     *
      * @param $prizeArray
      * @param Int $amount
      * @return Factory|View
      */
     private function insertToDB($prizeArray, Int $amount)
     {
-       $prize = new PrizesController();
-       $prize->loadToDB($prizeArray['id'], $amount);
-       return view('winpage', ['prize'=>$prizeArray['type'], 'amount'=>$amount]);
+        $prize = new PrizesController();
+        $prize->loadToDB($prizeArray['id'], $amount);
+        return view('winpage', ['prize' => $prizeArray['type'], 'amount' => $amount]);
+    }
+
+    /**
+     * Function to get a random item from DB
+     * @return array
+     */
+    public function getRandomItem()
+    {
+        return Thing::all()->where('status', '=', 1)->random()->toArray();
     }
 }
--- app/Http/Controllers/PrizesController.php
+++ app/Http/Controllers/PrizesController.php
@@ -3,17 +3,49 @@
 namespace App\Http\Controllers;
 
 use App\Models\Prizes;
-use Illuminate\Http\Request;
+use App\Models\PrizeType;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class PrizesController extends Controller
 {
     public function loadToDB($id, $amount)
     {
         $prize = new Prizes();
-        $prize->prize_type_id=$id;
-        $prize->amount=$amount;
-        $prize->user_id=Auth::id();
+        $prize->prize_type_id = $id;
+        $prize->amount = $amount;
+        $prize->user_id = Auth::id();
         $prize->save();
+    }
+
+    /**
+     * Функция, которая вернет массив с результатми всех игр
+     * @return array
+     */
+    public function summaryStats()
+    {
+        $reward = [];
+        foreach ($this->getAllPrizeTypes() as $item) {
+            $points = DB::table('prizes')->where([
+                    ['prize_type_id', '=', $item['id']],
+                    ['user_id', '=', Auth::id()],
+                ])
+                ->get()->sum('amount');
+
+            array_push($reward, [
+                    'type' => $item['type'],
+                    'quantity' => $points
+                ]);
+        }
+        return $reward;
+    }
+
+    /**
+     * Да, возможно отдельная функция лишняя.
+     * @return array
+     */
+    private function getAllPrizeTypes()
+    {
+        return (PrizeType::all()->toArray());
     }
 }
